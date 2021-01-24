c9af9711cf08da57303d8cd115fec5e0292bcfd8
Nicolas Kirchhoffer <git@nkirchho.dev>
2021-01-24 19:35:30 +0100

    Fix blacklist count

--- src/db/blacklist.js
+++ src/db/blacklist.js
@@ -5,13 +5,11 @@ const blacklist = {
     async add(url) {
         const gifs = db.get('blacklist');
 
-        const req = gifs.find({ url });
-
-        if (req.count() > 0) {
+        if (gifs.count({ url }) > 0) {
             throw new Error("Ce GIF a déjà été soumis à notre algorithme, mais merci quand même bg! :smiley:");
         }
 
-        const whitelist = db.get('whitelist').find({ url }).count();
+        const whitelist = db.get('whitelist').count({ url });
 
         if (whitelist > 0) {
             throw new Error("Ce GIF a déjà été soumis à notre algorithme, mais merci quand même bg! :smiley:");
