282cefdd9938e34180708882a95740f8bf41f502
Nicolas Kirchhoffer <git@nkirchho.dev>
2021-01-24 01:46:28 +0100

    Vérification de l'ingestion des messages

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const message = require('./src/message');
+
+const cleanChannels = [
+    process.env.WHITELIST_CHANNEL_ID,
+    process.env.BLACKLIST_CHANNEL_ID
+];
+
 bot.on('ready', () => {
     console.log('Monsieur Propre est prêt !');
 });
 
-bot.on('message', message => {
-    
+bot.on('message', msg => {
+    if (cleanChannels.indexOf(msg.channel.id) !== -1) {
+
+        message.checkMessage(msg.content).then(res => {
+            msg.react('✅');
+        }).catch(error => {
+            msg.author.send(error.message);
+            msg.delete();
+        });
+
+    }
 });
 
 bot.login(process.env.TOKEN);
---
+++ src/message.js
@@ -0,0 +1,38 @@
+const axios = require('axios').default;
+
+const message = {
+
+    regex: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/,
+
+    async checkMessage(content) {
+        if (!this.regex.test(content)) {
+            throw new Error('Le message que tu as posté ne comporte pas de lien :angry:');
+        }
+
+        const url = this.regex.exec(content)[0];
+        const extension = url.split('.');
+
+        if (extension[extension.length - 1] !== 'gif') {
+            throw new Error('Le lien que tu as posté ne représente pas d\'image GIF ! :angry:');
+        }
+        
+        try {
+            const res = await axios({
+                method: 'GET',
+                url,
+                timeout: '2000'
+            });
+    
+            if (res.headers['content-type'] !== 'image/gif') {
+                throw new Error('Le lien que tu as posté ne représente pas d\'image GIF valide (`image/gif`) :angry:');
+            }
+        } catch(error) {
+            throw new Error('Une erreur est survenue lorsque nous avons requêté ton lien, code : ' + error.code);
+        }
+
+        return true;
+    }   
+
+};
+
+module.exports = message;
